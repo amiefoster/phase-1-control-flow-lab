f8ab6be857ff40f2249cfab70d2558f84e151f0d
Amie Foster <amie.n.foster@gmail.com>
2021-08-30 11:49:22 -0600

    working on lab

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400){
+  return 'This one is on me!'
+
+  } else if (someValue >= 2000 && someValue < 2500) {
+    return 'I will gladly take your thirty bucks.'
+
+  } else if (someValue >= 2500) {
+      return 'No can do.'
+
+  }
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city = 'New York') {
+    return 'Ok, sounds good.'
+  }
+
+  else {
+    return 'No go.'  }
+
+  
 }
 
 function switchOnCharmFromTip(){
